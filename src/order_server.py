0eb7b1e4699e0504778b03bb56882c4e822e327d
Kamesh <Kamesh>
2020-04-04 22:47:42 -0700

    Fixed synchronization bug

--- src/order_server.py
+++ src/order_server.py
@@ -12,15 +12,21 @@ catalogue_addr = sys.argv[1]
 # Endpoint for the buy function
 @app.route('/buy/<item_no>', methods=['GET'])
 def buy(item_no):
+    sem.acquire()
     check_availability = requests.get('http://' + catalogue_addr + '/lookup/' + item_no)
     check_availability = check_availability.json()
     if(check_availability['stock'] == 0):
+        sem.release()
         return jsonify({
             'Buy Status': 'Error',
             'Reason': 'Item out of stock'
         })
     updated_book = requests.put('http://' + catalogue_addr + '/update/' + item_no, json={'stock': check_availability['stock'] - 1})
-    return updated_book.json()
+    sem.release()
+    return jsonify({
+        'Buy Status': 'Success',
+        'Item Bought': updated_book.json()['book_name']
+    })
 
 if __name__ == "__main__":
     app.run(port=5010, threaded=True)
