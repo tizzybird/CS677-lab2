ea5f29dd79ac76f78d2e041c0f832204c60c2d9e
tizzybird <yschen@livemail.tw>
2020-04-06 08:43:48 -0400

    Put log file path assignment into global settings

--- src/catalog_server.py
+++ src/catalog_server.py
@@ -13,6 +13,9 @@ with open('config.json') as f:
 HOST_IP = CONFIG['ip']['catalog']['addr']
 HOST_PORT = CONFIG['ip']['catalog']['port']
 
+log_search = CONFIG["log_path"]["folder_path"] + CONFIG["log_path"]["catalog_search"]
+log_lookup = CONFIG["log_path"]["folder_path"] + CONFIG["log_path"]["catalog_lookup"]
+log_buy    = CONFIG["log_path"]["folder_path"] + CONFIG["log_path"]["catalog_buy"]
 
 # This is the catalog. Rows are stored as [item_no, book_name, stock, cost, topic]
 catalog_file = sys.argv[1]
@@ -40,7 +43,7 @@ def search(topic):
     result = list(map(lambda book: {'item_no': book[0], 'book_name': book[1],'stock': book[2], 'cost': book[3], 'topic': book[4] }, result))
     sem.release()
     end_time = datetime.now()
-    with open('../tests/query_by_topic_times.txt', 'a') as f:
+    with open(log_search, 'a') as f:
         f.write('%f\n' % (end_time - start_time).total_seconds())
     return jsonify({
         'books': result
@@ -62,13 +65,14 @@ def lookup(item_no):
             break
     sem.release()
     end_time = datetime.now()
-    with open('../tests/query_by_item_times.txt', 'a') as f:
+    with open(log_lookup, 'a') as f:
         f.write('%f\n' % (end_time - start_time).total_seconds())
     return jsonify(res)
 
 # Endpoint for update
 @app.route('/update/<item_no>', methods=['PUT'])
 def update(item_no):
+    start_time = datetime.now()
     sem.acquire()
     req = request.get_json()
     for books in inventory:
@@ -76,8 +80,15 @@ def update(item_no):
             books[2] = req['stock']
             print("Stock for item " + item_no, "reduced to " + str(req['stock']), file = open('../tests/catalog_log.txt', 'a'))
             sem.release()
+            end_time = datetime.now()
+            
+            with open(log_buy, 'a') as f:
+                f.write('%f\n' % (end_time - start_time).total_seconds())
+            
             return jsonify({'item_no': books[0], 'book_name': books[1],'stock': books[2], 'cost': books[3], 'topic': books[4] })
+    
     sem.release()
+    return jsonify({}), 201
 
 
 if __name__ == '__main__':
